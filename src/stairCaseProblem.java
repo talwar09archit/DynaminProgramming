0c88122a057cf1cf4e4eb9fb116a7b901c66a570
talwar09archit <talwar09archit@gmail.com>
2021-08-01 18:52:35 +0530

    this is the first dynamic programming file

---
+++ src/stairCaseProblem.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class stairCaseProblem{
+    static int n;
+    static int[] dp = new int[100];
+    int solve(int n){
+        if(dp[n]!=0)
+            return dp[n];
+        if(n>=2)
+            dp[n]=solve(n-1)+solve(n-2);
+        return dp[n];
+    }
+    public static void main(String[] args){
+        stairCaseProblem obj = new stairCaseProblem();
+        System.out.print("Enter the number of stairs given: ");
+        Scanner data = new Scanner(System.in);
+        n = data.nextInt();
+        dp[0]=0;
+        dp[1]=1;
+        int answer=obj.solve(n);
+        System.out.print("The number of distinct ways in which one can travel is:"+answer);
+    }
+}
